cab47a00254b2dd8845deb22bd8b57d3df3bc572
Ryan Rustyn <ryanrustyn@gmail.com>
2022-09-21 16:17:57 -0700

    binary search trees

--- binary-tree.js
+++ binary-tree.js
@@ -19,12 +19,12 @@ class BinaryTree {
   minDepth(node = this.root, counter = 0, minCount = Infinity) {
     //count nodes until we reach a node without a child
     //have to remember count at ancestor node to add as diving down
-    
+
     //move from root to first node: count plus 1
     //move from node to first node on left: count plus 1
     //move from node to node with not children: count plus 1, total = 3, return to node above
-    
-    
+
+
     let stack = [node];
 
     while (stack.length) {
@@ -42,7 +42,7 @@ class BinaryTree {
         }
 
         if (current.left === null && current.right === null) {
-          if (minCount > counter) {  
+          if (minCount > counter) {
             minCount = counter;
           }
           return minCount;
@@ -56,15 +56,49 @@ class BinaryTree {
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
-  maxDepth(node = this.root) {
+  maxDepth(node = this.root, counter = 0, maxCount = -Infinity) {
+    let stack = [node];
 
+    while (stack.length) {
+      let current = stack.pop();
+      counter++;
+
+      if (current) {
+
+        if (current.left) {
+          maxCount = this.maxDepth(current.left, counter, maxCount);
+        }
+
+        if (current.right) {
+          maxCount = this.maxDepth(current.right, counter, maxCount);
+        }
+
+        if (current.left === null && current.right === null) {
+          if (maxCount < counter) {
+            maxCount = counter;
+          }
+          return maxCount;
+        }
+      }
+    }
+
+    return maxCount === -Infinity ? 0 : maxCount;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
-  nextLarger(lowerBound, node = this.root) {
+  nextLarger(lowerBound, node = this.root, answer = null) {
+    //is this node greater than lowerBound?
+    //is this node less than previous greater than lowerBound?
+    //check left, check right
 
+    if (node === null) return answer; //base case
+    if (node.val > lowerBound) { // answer > node > lowerbound
+      if (answer === null || answer > node.val) {
+        answer = node.val;
+      }
+    }
   }
 
   /** Further study!
---
+++ dsa-bsts/binary-search-tree.js
@@ -0,0 +1,104 @@
+class Node {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinarySearchTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** insert(val): Insert a new node into the BST with value val.
+   * Returns the tree. Uses iteration. */
+
+  insert(val) {
+    if (this.root === null) this.root = new Node(val);
+
+    let current = this.root;
+
+    while (current) {
+      if (current.left === null && val < current.val) {
+        current.left = new Node(val);
+      } else if (current.right === null && val > current.val) {
+        current.right = new Node(val);
+      }
+
+      current = (val < current.val)
+        ? current.left
+        : current.right;
+    }
+
+  }
+
+  /** insertRecursively(val): Insert a new node into the BST with value val.
+   * Returns the tree. Uses recursion. */
+
+  insertRecursively(val) {
+
+  }
+
+  /** find(val): Search the tree for a node with value val.
+   * Return the node, if found; else undefined. Uses iteration. */
+
+  find(val) {
+
+  }
+
+  /** findRecursively(val): Search the tree for a node with value val.
+   * Return the node, if found; else undefined. Uses recursion. */
+
+  findRecursively(val) {
+
+  }
+
+  /** dfsPreOrder(): Traverse the array using pre-order DFS.
+   * Return an array of visited nodes. */
+
+  dfsPreOrder() {
+
+  }
+
+  /** dfsInOrder(): Traverse the array using in-order DFS.
+   * Return an array of visited nodes. */
+
+  dfsInOrder() {
+
+  }
+
+  /** dfsPostOrder(): Traverse the array using post-order DFS.
+   * Return an array of visited nodes. */
+
+  dfsPostOrder() {
+
+  }
+
+  /** bfs(): Traverse the array using BFS.
+   * Return an array of visited nodes. */
+
+  bfs() {
+
+  }
+
+  /** findSuccessorNode(): Find the node with the next largest value.
+   * Return successor node or undefined if not found. */
+
+  findSuccessorNode(node) {
+
+  }
+
+  /** Further Study!
+   * remove(val): Removes a node in the BST with the value val.
+   * Returns the removed node. */
+
+  remove(val) {
+
+  }
+}
+
+module.exports = {
+  BinarySearchTree,
+  Node,
+};
