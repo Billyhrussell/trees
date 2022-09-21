196411b3de8ed70247b870f43f29a46403362704
Ryan Rustyn <ryanrustyn@gmail.com>
2022-09-21 12:31:34 -0700

    binary minlength

--- binary-tree.js
+++ binary-tree.js
@@ -16,8 +16,41 @@ class BinaryTree {
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth(node = this.root) {
+  minDepth(node = this.root, counter = 0, minCount = Infinity) {
+    //count nodes until we reach a node without a child
+    //have to remember count at ancestor node to add as diving down
+    
+    //move from root to first node: count plus 1
+    //move from node to first node on left: count plus 1
+    //move from node to node with not children: count plus 1, total = 3, return to node above
+    
+    
+    let stack = [node];
 
+    while (stack.length) {
+      let current = stack.pop();
+      counter++;
+
+      if (current) {
+
+        if (current.left) {
+          minCount = this.minDepth(current.left, counter, minCount);
+        }
+
+        if (current.right) {
+          minCount = this.minDepth(current.right, counter, minCount);
+        }
+
+        if (current.left === null && current.right === null) {
+          if (minCount > counter) {  
+            minCount = counter;
+          }
+          return minCount;
+        }
+      }
+    }
+
+    return minCount === Infinity ? 0 : minCount;
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
